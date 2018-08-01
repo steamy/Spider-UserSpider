d8d068bd56e895ff58d5a9541be255d7d18ed127
steamy <431917467@qq.com>
2018-08-01 23:40:26 +0800

    modified:   inspector.py

--- inspector.py
+++ inspector.py
@@ -12,37 +12,41 @@ import datetime
 import time
 import logging
 import os
+import json
 
 log_path = './log/watcher.log'
-os.makedirs('./log')
+if not os.path.exists('./log'):
+   os.makedirs('./log')
 f = open(log_path,'ab')
 f.close()
 LOG_FORMATE = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename=log_path, level=logging.INFO, format=LOG_FORMATE)
 
-
-
 def watcher():
-
+    f = open('./watcher.log','w+')
     # ip 代理池状态
-    #proxy_ip_status_cmd = 'curl http://127.0.0.1:5010/get_status/'
-    #proxy_ip_status = subprocess.Popen(proxy_ip_status_cmd, stdout = subprocess.PIPE, stdin = subprocess.PIPE, shell=True)
-    #print('【ip_proxy_pool】' + str(datetime.datetime.now()) + str(proxy_ip_status.communicate()[0]))
+    proxy_ip_status_cmd = 'curl http://127.0.0.1:5010/get_status/'
+    proxy_ip_status = subprocess.Popen(proxy_ip_status_cmd, stdout = subprocess.PIPE, stdin = subprocess.PIPE, shell=True)
+    logging.info('【ip_proxy_pool】'  + str(proxy_ip_status.communicate()[0]))
     # 爬取用户数
-    # r = Redis(connection_pool=redisPool)
-    # print('【reids】' + str(datetime.datetime.now()) + '已经爬取'+ str(r.scard('userid_used')) + '个用户');
-    # print('【reids】' + str(datetime.datetime.now()) + '还未爬取' + str(r.scard('userid_wanted')) + '个用户');
+    r = Redis(connection_pool=redisPool)
+    userid_used_num = r.scard('userid_used')
+    userid_wanted_num = r.scard('userid_wanted')
+    message = {
+        'userid_used_num': userid_used_num,
+        'userid_wanted_num': userid_wanted_num
+    }
+    logging.info('【reids】' + '已经爬取'+ str(userid_used_num) + '个用户')
+    logging.info('【reids】'+ '还未爬取' + str(userid_wanted_num) + '个用户')
+    r.publish('fetched_user_num_update', str(json.dumps(message)))
 
-    logging.info('test')
-
-
-# schedule.every(20).seconds.do(watcher)
+#schedule.every(10).minutes.do(watcher)
 
 if __name__ == '__main__':
-    # schedule.run_pending()
     while True:
-        watcher()
-        time.sleep(20)
+       #schedule.run_pending()
+       watcher()
+       time.sleep(300)
 
 
 
